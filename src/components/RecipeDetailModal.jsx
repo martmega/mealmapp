394fa152feca4a965fd7c0c59ba3c43bdd3cae7e
martmega <marty.clemen@gmail.com>
2025-06-15 19:25:43 +0200

    Refactor UI and fix profile date

--- src/components/RecipeDetailModal.jsx
+++ src/components/RecipeDetailModal.jsx
@@ -45,7 +45,7 @@ function RecipeDetailModal({ recipe, onClose, userProfile }) {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="fixed inset-0 flex items-center justify-center z-[60] p-4 bg-pastel-background/70 dark:bg-black/70"
+        className="fixed inset-0 flex items-center justify-center z-[60] p-4 bg-pastel-background/20 dark:bg-black/20"
         onClick={onClose}
       >
         <motion.div
