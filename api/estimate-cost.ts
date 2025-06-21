6e260d1094868a54f1a19c0cc8b6136904b51296
martmega <marty.clemen@gmail.com>
2025-06-21 13:25:55 +0200

    fix(api): use relative imports for utils

--- api/estimate-cost.ts
+++ api/estimate-cost.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import OpenAI from 'openai';
-import { getUserFromRequest } from '@/utils/auth';
+import { getUserFromRequest } from '../src/utils/auth';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
