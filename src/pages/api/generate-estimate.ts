bd3bd06a9342564e29d353e305565ec51acf6650
martmega <marty.clemen@gmail.com>
2025-06-21 14:18:01 +0200

    Add path alias configuration

--- src/pages/api/generate-estimate.ts
+++ src/pages/api/generate-estimate.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { generateRecipeEstimate } from '../../api/generateEstimate';
+import { generateRecipeEstimate } from '@/api/generateEstimate';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
