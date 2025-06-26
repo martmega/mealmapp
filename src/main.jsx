101bfd6c302b0732610e5c2ff6b450b31780db91
martmega <marty.clemen@gmail.com>
2025-06-26 13:47:15 +0200

    feat: add startup guard and reduce initial flash

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import '@/index.css';
+import AppBootstrapGuard from '@/components/layout/AppBootstrapGuard.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AppBootstrapGuard>
+        <App />
+      </AppBootstrapGuard>
     </BrowserRouter>
   </React.StrictMode>
 );
