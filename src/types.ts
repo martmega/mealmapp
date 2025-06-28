4cd07f80fceda7ff0f4f68e7d0d0e975b7fbb939
martmega <marty.clemen@gmail.com>
2025-06-28 16:54:35 +0200

    Allow multiple meal types per meal

--- src/types.ts
+++ src/types.ts
@@ -9,7 +9,7 @@ export type WeeklyMenuPreferences = {
   portions_per_meal: number;
   daily_calories_limit: number | null;
   weekly_budget: number;
-  daily_meal_structure: string[];
+  daily_meal_structure: string[][];
   tag_preferences: string[];
   /** Client side representation of meals */
   meals?: {
