960e8044fe981ab9dc83d6231c0cdfd04fe82322
martmega <marty.clemen@gmail.com>
2025-06-28 16:11:12 +0200

    refactor: centralize default menu preferences

---
+++ src/lib/defaultPreferences.js
@@ -0,0 +1,8 @@
+export const DEFAULT_MENU_PREFS = {
+  servingsPerMeal: 4,
+  maxCalories: 2200,
+  weeklyBudget: 35,
+  meals: [],
+  tagPreferences: [],
+  commonMenuSettings: { enabled: true, linkedUsers: [], linkedUserRecipes: [] },
+};
