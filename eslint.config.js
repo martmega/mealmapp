6c839a762910fddc319b1173bd08d1fbdaf89ec8
martmega <marty.clemen@gmail.com>
2025-07-04 10:54:28 +0200

    Add ESLint config and update lint script

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react: reactPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+    },
+  },
+];
